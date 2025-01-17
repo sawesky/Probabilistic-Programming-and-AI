5319da557b5b1ba0456a83480d8823c0e9f081ee
Vladimir Pezo <pezovladimir2@gmail.com>
2025-01-17 14:13:27 +0100

    add convolution to encoder and decoder

--- CVAE/main.py
+++ CVAE/main.py
@@ -7,6 +7,7 @@ import baseline
 import cvae
 import pandas as pd
 import torch
+import os
 from util import (
     generate_table,
     get_data,
@@ -63,16 +64,24 @@ def main(args):
             raise ValueError("Dataset not supported")
 
         # Train baseline
-        baseline_net = baseline.train(
-            device=device,
-            dataloaders=dataloaders,
-            dataset_sizes=dataset_sizes,
-            learning_rate=args.learning_rate,
-            num_epochs=args.num_epochs,
-            early_stop_patience=args.early_stop_patience,
-            model_path="baseline_net_q{}.pth".format(num_quadrant_inputs),
-            dataset=args.dataset,
-        )
+        if args.allow_baseline_reuse and os.path.exists(
+            "baseline_net_q{}.pth".format(num_quadrant_inputs)
+        ):
+            print("Reusing pre-trained baseline model")
+            baseline_net = torch.load(
+                "baseline_net_q{}.pth".format(num_quadrant_inputs)
+            )
+        else:
+            baseline_net = baseline.train(
+                device=device,
+                dataloaders=dataloaders,
+                dataset_sizes=dataset_sizes,
+                learning_rate=args.learning_rate,
+                num_epochs=args.num_epochs,
+                early_stop_patience=args.early_stop_patience,
+                model_path="baseline_net_q{}.pth".format(num_quadrant_inputs),
+                dataset=args.dataset,
+            )
 
         # Train CVAE
         cvae_net = cvae.train(
@@ -82,12 +91,17 @@ def main(args):
             learning_rate=args.learning_rate,
             num_epochs=args.num_epochs,
             early_stop_patience=args.early_stop_patience,
-            model_path="cvae_net_q{}.pth".format(num_quadrant_inputs),
+            model_path="cvae_net_q{}{}{}.pth".format(
+                num_quadrant_inputs,
+                "_rand_mask" if args.random_mask else "",
+                "_conv" if args.use_conv else "",
+            ),
             pre_trained_baseline_net=baseline_net,
             dataset=args.dataset,
             z_dim=args.z_dim,
             hidden_1=args.hidden_1,
             hidden_2=args.hidden_2,
+            use_conv=args.use_conv,
         )
 
         # Visualize conditional predictions
@@ -109,8 +123,9 @@ def main(args):
                 pre_trained_cvae=cvae_net,
                 num_images=args.num_images,
                 num_samples=args.num_samples,
-                image_path="cvae{}_plot_q{}.png".format(
+                image_path="cvae{}{}_plot_q{}.png".format(
                     "_rand_mask" if args.random_mask else "",
+                    "_conv" if args.use_conv else "",
                     num_quadrant_inputs,
                 ),
                 random_mask=args.random_mask,
@@ -253,6 +268,13 @@ if __name__ == "__main__":
         action="store_true",
         default=False,
         help="random mask",
+    )
+    parser.add_argument(
+        "-uc",
+        "--use-conv",
+        action="store_true",
+        default=False,
+        help="use convolutional neural network",
     )
     args = parser.parse_args()
 
